ee181d09fd8b80ab752e7d341f131868b9d024cd
Petter10 <66138739+Petter10@users.noreply.github.com>
2020-05-29 14:11:26 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
    url: 'http://localhost:5000/firestore-grafica-1bc3e/us-central1',
    firebase: {
-    apiKey: 'AIzaSyA5d7Naxmry0DIh9d4C9xqW7OxtianVeYI',
+    apiKey: 'asdSsDasAadAdw123dssa',
     authDomain: 'firestore-grafica-1bc3e.firebaseapp.com',
     databaseURL: 'https://firestore-grafica-1bc3e.firebaseio.com',
     projectId: 'firestore-grafica-1bc3e',
     storageBucket: 'firestore-grafica-1bc3e.appspot.com',
-    messagingSenderId: '757758647665',
-    appId: '1:757758647665:web:22cc8a0d262fccecf2ead0'
+    messagingSenderId: '7577586471111',
+    appId: 'XX'
   }
 };
 
